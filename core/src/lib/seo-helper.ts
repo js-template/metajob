61424f6f5cd46e3c51e4261eeed87ef654c24832
mostafizur <mostafizur.mr33@gmail.com>
2025-05-28 06:54:37 +0600

    dynamic public page jsonld added

--- core/src/lib/seo-helper.ts
+++ core/src/lib/seo-helper.ts
@@ -13,12 +13,24 @@ export type JsonLdBase = {
  * @returns A full JSON-LD object
  */
 export const jsonLdFormatter = (data: Record<string, any>, type: string = "WebSite"): JsonLdBase => {
+   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ""
+
+   if (type === "ItemList") {
+      return {
+         "@context": "https://schema.org",
+         "@type": "ItemList",
+         name: data?.metaTitle || "",
+         description: data?.metaDescription || "",
+         url: data?.canonicalURL || baseUrl || ""
+      }
+   }
+
    return {
       "@context": "https://schema.org",
       "@type": type,
       name: data?.metaTitle || "",
       description: data?.metaDescription || "",
-      url: process.env.NEXT_PUBLIC_BASE_URL || "",
+      url: data?.canonicalURL || baseUrl || "",
       thumbnailUrl: data?.metaImage?.url || ""
    }
 }
